601fb37e0690d1509f3ddf207f922bbde9ca4d94
Aliaksandra <ryzhikai@rambler.ru>
2022-01-16 21:30:13 +0300

    first commit

---
+++ src/main/java/config/UserConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+public class UserConfig {
+    public static final String USER_NAME = "AliaksandraRyzhyk";
+    public static final String USER_LOGIN = "DemoTest0@list.ru";
+    public static final String USER_PASSWORD = "Password!1";
+    public static final String USER_CONFIRM_PASSWORD = "Password!1";
+}
