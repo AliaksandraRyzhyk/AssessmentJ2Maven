1c9a32dd97448cb7a482402ba46d166d975132c2
Aliaksandra <77581718+AliaksandraRyzhyk@users.noreply.github.com>
2022-01-21 10:57:33 +0300

    Update UserConfig.java

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -2,7 +2,7 @@ package config;
 
 public class UserConfig {
     public static final String USER_NAME = "AliaksandraRyzhyk";
-    public static final String USER_LOGIN = "DemoTest0@list.ru";
+    public static final String USER_LOGIN = RandomStringUtils.randomAlphabetic(6)+"@list.ru";
     public static final String USER_PASSWORD = "Password!1";
     public static final String USER_CONFIRM_PASSWORD = "Password!1";
 }
