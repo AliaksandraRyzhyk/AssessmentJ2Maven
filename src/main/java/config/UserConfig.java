a63a2f78bb01a6b555ccf86d602bf6d1e61915b6
Aliaksandra <77581718+AliaksandraRyzhyk@users.noreply.github.com>
2022-04-05 19:57:00 +0300

    Update UserConfig.java

--- src/main/java/config/UserConfig.java
+++ src/main/java/config/UserConfig.java
@@ -1,5 +1,7 @@
 package config;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class UserConfig {
     public static final String USER_NAME = "AliaksandraRyzhyk";
     public static final String USER_LOGIN = RandomStringUtils.randomAlphabetic(6)+"@list.ru";
